d75ab96f92d100582161778a3c41443a086f571d
Kyle Van Meter <kvanmete76751@gmail.com>
2020-03-15 23:39:34 -0700

    DataAccessLayer: Added two cases for ConvertTime test

--- DataAccessLayerTests.cs
+++ DataAccessLayerTests.cs
@@ -7,6 +7,12 @@ namespace DoNothing.Tests
 {
     public class DataAccessLayerTests
     {
+        /// <summary>
+        /// ConvertTime takes a TimeSpan object and converts it into a string representation.
+        /// Depending on the context we append a "0" to part of the string 
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="expected"></param>
         [Trait("DataAccessLayer", "ConvertTime Simple")]
         [Theory]
         [MemberData(nameof(TimeSpanData))]
@@ -25,7 +31,9 @@ namespace DoNothing.Tests
                 new object[] { new TimeSpan(0,12,13), "12:13" },
                 new object[] { new TimeSpan(0,3,10), "3:10" },
                 new object[] { new TimeSpan(0,16,1), "16:01" },
-                new object[] { new TimeSpan(1,33,7), "1:33:07" }
+                new object[] { new TimeSpan(1,33,7), "1:33:07" },
+                new object[] { new TimeSpan(0,0,8), "0:08" },
+                new object[] { new TimeSpan(1,8,7), "1:08:07"}
             };
     }
 }

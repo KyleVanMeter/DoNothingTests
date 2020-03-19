7f09e0419b3ff5e8ce4c64076fcebf98013f232a
Kyle Van Meter <kvanmete76751@gmail.com>
2020-03-18 21:47:14 -0700

    DataAccessLayer: Added GetAlbumArt unit test for ext

--- DataAccessLayerTests.cs
+++ DataAccessLayerTests.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Reflection;
 
 using Xunit;
 using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 using test02.Models;
 
 namespace DoNothing.Tests
 {
     public class DataAccessLayerTests
     {
+        private readonly Xunit.Abstractions.ITestOutputHelper _testOutput;
+
+        public DataAccessLayerTests(Xunit.Abstractions.ITestOutputHelper _testOutput)
+        {
+            this._testOutput = _testOutput;
+        }
+
+        public string CurrentDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
         /// <summary>
         /// ConvertTime takes a TimeSpan object and converts it into a string representation.
         /// Depending on the context we append a "0" to part of the string 
@@ -70,7 +81,7 @@ namespace DoNothing.Tests
         [InlineData("unused", null)]
         public void GetAlbumArtTestExt(string folder, string expected)
         {
-            string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string path = CurrentDir;
             string dirName = @"testDir";
 
             System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Path.Combine(path, dirName));
@@ -88,5 +99,50 @@ namespace DoNothing.Tests
 
             Assert.Equal(expected, result);
         }
+
+        [Trait("DataAccessLayer", "GetAblumArt Ext2")]
+        [Fact]
+        public void GetAlbumArtTestExt2()
+        {
+            string path = CurrentDir;
+            string dirName = @"testDir";
+
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Path.Combine(path, dirName));
+            string expected = dir.FullName + @"\cover.jpg";
+
+            if (dir.Exists)
+            {
+                dir.Delete();
+            }
+
+            dir.Create();
+
+            try
+            {
+                using (FileStream fs = File.Create(dir.FullName + @"\cover.jpg"))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("test");
+                    fs.Write(info, 0, info.Length);
+                }
+            } catch (Exception e)
+            {
+                _testOutput.WriteLine("File creation failed with exception: " + e.ToString());
+            }
+
+            var comp = new DataAccessLayer();
+
+            var result = comp.GetAlbumArt(dir.FullName);
+
+            dir.Delete(true);
+
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetAlbumArtFileData =>
+            new List<object[]>
+            {
+                new object[] { @"\cover.jpg", System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).EndsWith("cover.jpg") },
+                new object[] { new TimeSpan(1,8,7), "1:08:07"}
+            };
     }
 }
